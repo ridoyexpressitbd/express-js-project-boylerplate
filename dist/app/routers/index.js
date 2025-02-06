af747b84a35a570a0538caccf0e8f92d57468a8a
ridoyexpressitbd <ridoy@expressitbd.dev>
2025-02-06 12:46:28 -0500

    modify temple

---
+++ dist/app/routers/index.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const auth_route_1 = require("../modules/auth/auth.route");
+const user_route_1 = require("../modules/users/user.route");
+const routers = express_1.default.Router();
+const moduleRoutes = [
+    {
+        path: '/auth',
+        route: auth_route_1.AuthRoutes
+    },
+    {
+        path: '/user',
+        route: user_route_1.UserRoutes
+    }
+];
+moduleRoutes.forEach(route => {
+    routers.use(route.path, route.route);
+});
+exports.default = routers;
